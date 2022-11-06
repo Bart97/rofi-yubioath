63fcebff51dbfdb8838757e189f409257fe0fb75
Bartosz Kazmierczak <kazmierczakbartosz97@gmail.com>
2022-11-06 17:07:55 +0100

    Initial commit

---
+++ rofi-yubioath.py
@@ -0,0 +1,59 @@
+from ykman.device import connect_to_device
+from yubikit.core.smartcard import SmartCardConnection
+import yubikit.oath
+import subprocess
+import pyperclip
+
+def creds_to_string_dict(creds):
+    converted = {}
+    for cred, code in creds.items():
+        if code is not None:
+            converted[cred.id.decode("utf-8")] = code.value
+        else:
+            converted[cred.id.decode("utf-8")] = ""
+    return converted
+
+def get_longest_cred_name(credDict):
+    return len(max(credDict.keys(), key=len))
+
+def build_creds_string(credDict):
+    longestCred = get_longest_cred_name(credDict)
+    credsString = ''
+    for cred, code in credDict.items():
+        credsString += cred.ljust(longestCred, ' ') + ','
+        credsString += code
+        credsString += '|'
+    credsString = credsString[:-1]
+    return credsString
+
+
+connection, device, info = connect_to_device(
+        connection_types=[SmartCardConnection])
+
+session = yubikit.oath.OathSession(connection)
+creds = session.calculate_all()
+
+credDict = creds_to_string_dict(creds)
+
+credsString = build_creds_string(credDict)
+
+result = subprocess.run(
+        [
+            'rofi',
+            '-dmenu',
+            '-sep', "|",
+            '-display-column-separator', ',',
+            '-display-columns', '1,2',
+            '-no-custom',
+            '-format', 'i'
+        ], input=credsString,
+        capture_output=True, text=True)
+
+if result.returncode == 0:
+    try:
+        selectedCred = int(result.stdout)
+        code = session.calculate_code(list(creds.keys())[selectedCred])
+        pyperclip.copy(code.value)
+    except ValueError:
+        pass
+
